c654f57e6179cfde6c6b2638cb0ebad9be1a23e3
Nohavye <noha.poncelet@gmail.com>
2024-09-06 16:18:47 +0200

    feat(API): Add '/prefer_dark_theme' endpoint

--- vx_shell/api/endpoints/basics.py
+++ vx_shell/api/endpoints/basics.py
@@ -1,3 +1,4 @@
+from fastapi.responses import JSONResponse
 from ..api import api
 from ...servers import ApiServer
 
@@ -11,3 +12,19 @@ async def ping():
 async def close():
     ApiServer.server.should_exit = True
     return
+
+
+import gi
+
+gi.require_version("Gtk", "3.0")
+from gi.repository import Gtk
+
+
+@api.get(
+    "/prefer_dark_theme",
+    description="Return the 'gtk-application-prefer-dark-theme' gtk property",
+)
+async def prefer_dark_theme():
+    settings = Gtk.Settings.get_default()
+
+    return JSONResponse(settings.get_property("gtk-application-prefer-dark-theme"))
